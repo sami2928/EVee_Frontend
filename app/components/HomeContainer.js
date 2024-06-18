626a810b5c84e9683f1ec3ad3dab5ae544908ad4
sami2928 <samiulhaq2928@hotmail.com>
2024-06-18 13:49:19 +0500

    Added Navbar

---
+++ app/components/HomeContainer.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+    StyleSheet,
+    KeyboardAvoidingView,
+    ScrollView,
+    Dimensions,
+    Image,
+} from "react-native";
+import Navbar from './Navbar';
+
+const HomeContainer = ({ children, onHomePress, onProfilePress, onLogoutPress }) => {
+    return (
+        <KeyboardAvoidingView style={styles.container}>
+            <Navbar
+                onHomePress={onHomePress}
+                onProfilePress={onProfilePress}
+                onLogoutPress={onLogoutPress}
+            />
+            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.scrollView}>
+                <Image style={styles.logo} source={require("../assets/logo.png")} />
+                {children}
+            </ScrollView>
+        </KeyboardAvoidingView>
+    );
+};
+
+const { height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+    },
+    scrollView: {
+        alignItems: "center",
+    },
+    logo: {
+        height: 125,
+        width: 125,
+        marginBottom: 20,
+        marginTop: height * 0.30,
+        alignSelf: "center",
+    },
+});
+
+export default HomeContainer;
--- app/components/screens/Home.js
+++ app/components/screens/Home.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {StackActions, useNavigation} from '@react-navigation/native';
+import HomeContainer from '../HomeContainer';
 
 const Home = ({route}) => {
-  const {profile} = route.params;
+  const profile = route.params ? route.params.profile : null;
+  const navigation = useNavigation();
+
+  const handleHomePress = () => {
+    console.log('profile: ', profile);
+    navigation.dispatch(StackActions.replace('Home', {profile: profile}));
+  };
+
+  const handleProfilePress = () => {
+    // Navigate to the Profile screen or perform profile-related actions
+  };
+
+  const handleLogoutPress = () => {
+    navigation.dispatch(StackActions.replace('SignIn'));
+  };
 
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+      <HomeContainer
+          onHomePress={handleHomePress}
+          onProfilePress={handleProfilePress}
+          onLogoutPress={handleLogoutPress}
+      >
+        <View style={styles.content}>
+          <Text>Welcome to the Home Screen</Text>
+        </View>
+      </HomeContainer>
   );
 };
 
+const {width} = Dimensions.get('window');
+
 const styles = StyleSheet.create({
-  container: '',
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
+
 export default Home;
